eba268a735f8c2d9f8f20c9a72c177f32f755cd6
unknown <salatsajiya7.8.6@gmail.com>
2022-09-06 08:26:00 +0530

    MCQ App

---
+++ app/src/main/java/com/example/practiceset/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.practiceset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+private String[] questions = {"Java is a person?","Java was introduced in 1233?","Java was created using Python","Java has abstract classes?" ,
+        "Java supports interface?"};
+private boolean[] answers ={false,false,false,true,true};
+private int score =0;
+    Button yes;
+    Button no;
+    TextView question;
+private int index =0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        yes = findViewById(R.id.yes);
+        no = findViewById(R.id.no);
+        question = findViewById(R.id.question);
+        question.setText(questions[index]);
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(index<=questions.length-1) {
+                    if(answers[index]== true){
+                        score++;
+                    }
+                    index ++;
+                    if(index<=questions.length-1){
+                        question.setText(questions[index]);
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Your Score is"+ score + "/"+ questions.length, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Restart the app to play again ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // If the array is not going out of bound
+
+            if(index<=questions.length-1) {
+                if(answers[index]==false){
+                    score++;
+           }
+                index ++;
+                if(index<=questions.length-1){
+                    question.setText(questions[index]);
+                }
+                else{
+                Toast.makeText(MainActivity.this, "Your Score is"+ score + "/"+ questions.length, Toast.LENGTH_SHORT).show();
+                }
+           }
+            else{
+                Toast.makeText(MainActivity.this, "Your Score is"+ score, Toast.LENGTH_SHORT).show();
+            }
+            }
+        });
+
+
+    }
+}
